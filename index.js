c5089a84daeaf6f866f6ec445b9cd551160bd0ec
tayga2410 <moldazhanov.eldar@gmail.com>
2024-09-05 19:25:24 +0500

    copy of repo

---
+++ index.js
@@ -0,0 +1,18 @@
+const calculateRPN = require('@tayga2410/node_1-mod'); 
+
+const examples = [
+  '15 9 + 10 - 14 - 7 * 5 14 * 14 - *',
+  '8 2 15 * + 8 - 13 * 15 - 6 6 13 + + -',
+  '4 9 + 1 - 2 6 13 8 0 6 9 * - - + + - *',
+  '6 1 - 14 * 11 + 3 - 2 12 5 - * +',
+  '2 11 6 * - 6 * 7 - 7 + 14 + 8 10 * +',
+  '13 3 14 * 10 + * 12 4 + + 8 *',
+  '10 3 11 - + 7 + 2 - 10 - 10 -',
+  '4 9 - 1 14 - - 12 + 2 1 - - 8 -',
+  '2 11 0 * * 13 15 - + 0 - 4 * 1 *',
+  '4 8 6 10 * * * 0 11 * -'
+];
+
+examples.forEach(expression => {
+  console.log(`Result of "${expression}":`, calculateRPN(expression));
+});
